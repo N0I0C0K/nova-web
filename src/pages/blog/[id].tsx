38d17fad975e9b0cc6650ce4234a8060f3f5902e
N0I0C0K <nick131410@aliyun.com>
2023-07-30 13:05:38 +0800

    添加了markdown支持，添加了自定义md元素渲染

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -2,14 +2,25 @@ import { ArticleProps } from '@/types'
 import { Box, Flex, Heading } from '@chakra-ui/react'
 import { GetServerSideProps } from 'next'
 import { FC } from 'react'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import fs from 'fs'
+import CustomRenderer from '@/components/markdown/CustomRenderer'
 
 const ArticlePage: FC<{
   article: ArticleProps
-}> = ({ article }) => {
+  content: string
+}> = ({ article, content }) => {
   return (
     <Flex pt={'5rem'} flexDirection={'column'} className='items-center'>
       <Box w={'50vw'}>
         <Heading>{article.title}</Heading>
+        <ReactMarkdown
+          components={CustomRenderer()}
+          remarkPlugins={[remarkGfm]}
+        >
+          {content}
+        </ReactMarkdown>
       </Box>
     </Flex>
   )
@@ -18,12 +29,14 @@ const ArticlePage: FC<{
 export const getServerSideProps: GetServerSideProps<
   {
     article: ArticleProps
+    content: string
   },
   {
     id: string
   }
 > = async (ctx) => {
   const { id } = ctx.params!
+  const fileContent = fs.readFileSync('./README.md', 'utf-8')
   return {
     props: {
       article: {
@@ -33,6 +46,7 @@ export const getServerSideProps: GetServerSideProps<
         badges: ['badge1', 'badge2'],
         author: 'nick',
       },
+      content: fileContent,
     },
   }
 }
