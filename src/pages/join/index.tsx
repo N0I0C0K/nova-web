48bb01d45436a57dee278a5a0fb05c5e4fe5304b
N0I0C0K <nick131410@aliyun.com>
2023-07-31 23:44:17 +0800

    美化了join页面

--- src/pages/join/index.tsx
+++ src/pages/join/index.tsx
@@ -9,11 +9,33 @@ import {
 } from '@chakra-ui/react'
 import { GetServerSideProps } from 'next'
 import { Formik, Form, Field } from 'formik'
+//import { Image } from '@chakra-ui/next-js'
+import Image from 'next/image'
+import backImg from './back.png'
 
 const JoinPage = () => {
   return (
-    <Flex pt={'5rem'} flexDir={'column'} className='items-center'>
-      <Heading>Join Us</Heading>
+    <Flex flexDir={'column'} className='items-center' pos={'relative'}>
+      <Flex
+        pos={'relative'}
+        w={'50rem'}
+        h={'10rem'}
+        mt={'5rem'}
+        className='items-center justify-center'
+      >
+        <Image
+          src={backImg}
+          alt='11'
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+          className='object-cover rounded-xl absolute brightness-90 shadow-xl'
+        />
+        <Heading zIndex={1} colorScheme='blackAlpha'>
+          Join Us
+        </Heading>
+      </Flex>
       <Formik
         initialValues={{
           name: '',
@@ -24,7 +46,7 @@ const JoinPage = () => {
         }}
       >
         <Form
-          className='flex flex-col items-center gap-3'
+          className='flex flex-col items-center gap-3 mt-10'
           style={{
             width: '30rem',
           }}
@@ -45,7 +67,7 @@ const JoinPage = () => {
               </FormControl>
             )}
           </Field>
-          <Button type='submit' colorScheme='blue' width={'100%'}>
+          <Button type='submit' colorScheme='blue' width={'100%'} mt={'3rem'}>
             Submit!
           </Button>
         </Form>
