6f98cce4ce92dea005fd382b2e100e85053642be
nickxizhang <nick131410@aliyun.com>
2023-09-01 14:53:54 +0800

    feat：美化了member界面

--- src/pages/member/index.tsx
+++ src/pages/member/index.tsx
@@ -1,7 +1,7 @@
 import { prisma } from '@/db'
 import { MemberProps } from '@/types'
 import { Link } from '@chakra-ui/next-js'
-import { Avatar, Box, Flex, Heading, Text } from '@chakra-ui/react'
+import { Avatar, Box, Flex, Heading, Image, Text } from '@chakra-ui/react'
 import { GetServerSideProps } from 'next'
 
 const MemberCard: React.FC<MemberProps> = (member) => {
@@ -31,6 +31,31 @@ const MemberCard: React.FC<MemberProps> = (member) => {
   )
 }
 
+function GroupColumn({ groupName }: { groupName: string }) {
+  return (
+    <Flex
+      flexDir={'column'}
+      className='rounded-lg p-3 items-center justify-center duration-500 group hover:w-72 '
+      w={'10rem'}
+      pos={'relative'}
+      shadow={'lg'}
+    >
+      <Heading color={'white'}>{groupName}</Heading>
+      <Image
+        alt='back'
+        src='https://nova-1257272505.cos.ap-guangzhou.myqcloud.com/Join.png'
+        pos={'absolute'}
+        zIndex={-1}
+        height={'100%'}
+        width={'100%'}
+        objectFit={'cover'}
+        rounded={'md'}
+        className='brightness-90'
+      />
+    </Flex>
+  )
+}
+
 const MemberPage: React.FC<{
   members: MemberProps[]
 }> = ({ members }) => {
@@ -41,10 +66,11 @@ const MemberPage: React.FC<{
       className='items-center'
       gap={'2rem'}
     >
-      <Heading>Member</Heading>
-      {members.map((member) => (
-        <MemberCard {...member} key={member.id} />
-      ))}
+      <Flex gap={'1rem'} h={'80vh'}>
+        <GroupColumn groupName='策划' />
+        <GroupColumn groupName='程序' />
+        <GroupColumn groupName='美术' />
+      </Flex>
     </Flex>
   )
 }
